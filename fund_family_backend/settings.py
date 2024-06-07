5504917e62c2804008e834611dbaf01fe417b0c5
yuenfu001 <liuyuenfu@gmail.com>
2024-06-07 18:25:35 +0100

    added url to alloed host

--- fund_family_backend/settings.py
+++ fund_family_backend/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = "vxsWujapNDP430W3PyScejgbmydt3bJP9hbKptHf_HKTRfI2EyGF-4hI-xEVdfZY9I
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
-
+ALLOWED_HOSTS = ["fund-family-backend-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ['https://fund-family-backend-production.up.railway.app']
 
 # Application definition
 
