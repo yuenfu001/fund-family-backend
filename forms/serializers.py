a8eb9ccd74cc3a1f63cf30257192bd2bfffe1388
yuenfu001 <liuyuenfu@gmail.com>
2024-06-24 20:13:53 +0100

    removed approval from serializer

--- forms/serializers.py
+++ forms/serializers.py
@@ -7,7 +7,7 @@ class formDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = formData
         fields = [
-        "name","email","no_of_family","raised_amount","fund_breakdown","fund_url","comments","family_in_egypt","approval"
+        "name","email","no_of_family","raised_amount","fund_breakdown","fund_url","comments","family_in_egypt"
         ]
 class getDataSerializer(serializers.ModelSerializer):
     class Meta:
