790d5493081abf4d6937a7c9285570bfe9082062
yuenfu001 <liuyuenfu@gmail.com>
2024-06-25 23:01:32 +0100

    adjusted serializers and views

--- forms/urls.py
+++ forms/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("allform/", views.getAllForm, name="get_all_form"),
     path("create_form/", views.createForm, name="create_form"),
     path("get_only_approved/", views.getAllApprovedForm, name="get_only"),
-    path("approve_form/", views.getApproval, name="approve_form"),
+    path("approve_form/<str:pk>/", views.getApproval, name="approve_form"),
     # path("get_form/<str:pk>/", views.specificForm, name="specific_form"),
 ]
