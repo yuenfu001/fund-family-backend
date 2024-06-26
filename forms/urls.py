6f69b8eda58ed8caf902d227d37445f20efe6344
yuenfu001 <liuyuenfu@gmail.com>
2024-06-26 14:34:40 +0100

    re-arranged urls

--- forms/urls.py
+++ forms/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("allform/", views.getAllForm, name="get_all_form"),
-    path("create_form/", views.createForm, name="create_form"),
-    path("get_only_approved/", views.getAllApprovedForm, name="get_only"),
     path("approve_form/<str:pk>/", views.getApproval, name="approve_form"),
+    path("create_form/", views.createForm, name="create_form"),
+    path("allform/", views.getAllForm, name="get_all_form"),
+    path("get_only_approved/", views.getAllApprovedForm, name="get_only"),
     # path("get_form/<str:pk>/", views.specificForm, name="specific_form"),
 ]
