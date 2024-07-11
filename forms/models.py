6863112f222269505746bc946e22cb0feefb6d37
yuenfu001 <liuyuenfu@gmail.com>
2024-07-11 17:28:07 +0100

    removed default for family_in_egypt

--- forms/models.py
+++ forms/models.py
@@ -7,7 +7,7 @@ class formData(models.Model):
     no_of_family = models.PositiveIntegerField(default=0)
     raised_amount = models.DecimalField(default=0.0, decimal_places=2, max_digits=10)
     fund_breakdown = models.TextField()
-    family_in_egypt = models.BooleanField(default=True)
+    family_in_egypt = models.BooleanField()
     fund_url = models.CharField(max_length=255)
     comments = models.TextField()
     approval = models.BooleanField(default=False)
